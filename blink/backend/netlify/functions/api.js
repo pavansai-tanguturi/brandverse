9b66d8aa7193121861550a378f8aec771ebbfc96
pavansai-tanguturi <svntpavansai@gmail.com>
2025-09-17 17:20:01 +0530

    feat: Remove outdated Netlify configuration and serverless function; add new frontend deployment settings

--- blink/backend/netlify/functions/api.js
+++
@@ -1,7 +0,0 @@
-import serverlessExpress from '@vendia/serverless-express';
-import app from '../../server.js';
-
-// Create serverless handler
-const handler = serverlessExpress({ app });
-
-export { handler };
